cf6f8ca21e0fde522b347eb1294fdea7ab2f7dad
@zwken123456 <2979602290@qq.com>
2025-06-03 22:52:32 +0800

    [feat] 打通调度图

--- Hydra/hydra-framework-runtime/src/main/java/com/pinecone/hydra/atlas/advance/strategy/InDegreeFirstStrategy.java
+++ Hydra/hydra-framework-runtime/src/main/java/com/pinecone/hydra/atlas/advance/strategy/InDegreeFirstStrategy.java
@@ -3,12 +3,15 @@ package com.pinecone.hydra.atlas.advance.strategy;
 import com.pinecone.framework.util.id.GUID;
 import com.pinecone.hydra.atlas.graph.RuntimeAtlasInstrument;
 import com.pinecone.hydra.task.kom.entity.TaskElement;
+import com.pinecone.hydra.unit.imperium.entity.TreeNode;
 import com.pinecone.hydra.unit.iqueue.MegaDeflectPriorityQueue;
 import com.pinecone.hydra.unit.iqueue.MegaStratumQueue;
 import com.pinecone.hydra.unit.iqueue.entity.GenericQueueElement;
 import com.pinecone.hydra.unit.iqueue.entity.GenericStratumQueueElement;
 import com.pinecone.hydra.unit.iqueue.entity.QueueStratumElement;
 import com.pinecone.hydra.unit.vgraph.VectorDAG;
+import com.pinecone.hydra.unit.vgraph.layer.Layer;
+import com.pinecone.hydra.unit.vgraph.layer.LayerInstrument;
 
 import java.util.List;
 
@@ -19,28 +22,32 @@ public class InDegreeFirstStrategy implements GraphPriorityProcessStrategy {
 
     private MegaStratumQueue            mTempMegaStratumQueue;
 
+    private LayerInstrument             mLayerInstrument;
+
     private int mnPriority = 0;
 
     public InDegreeFirstStrategy(RuntimeAtlasInstrument runtimeAtlasInstrument, MegaDeflectPriorityQueue megaDeflectPriorityQueue,
-                                 MegaStratumQueue megaStratumQueue ) {
+                                 MegaStratumQueue megaStratumQueue, LayerInstrument layerInstrument ) {
         this.mRuntimeAtlasInstrument    = runtimeAtlasInstrument;
         this.mMegaDeflectPriorityQueue  = megaDeflectPriorityQueue;
-        this.mTempMegaStratumQueue = megaStratumQueue;
+        this.mTempMegaStratumQueue      = megaStratumQueue;
+        this.mLayerInstrument           = layerInstrument;
     }
 
 
     @Override
     public void process( VectorDAG vectorDAG ) {
-        long handNodeNums = vectorDAG.countHandleNodes();
+        Layer layer = (Layer)this.mLayerInstrument.get(vectorDAG.getAffiliateLayerGuid());
+        long handNodeNums = this.mLayerInstrument.countSourceNode( vectorDAG.getAffiliateLayerGuid() );
         long offset = 0;
 
-        //todo后面记得将这个每次遍历的节点数量改成配置
+        //todo 后面记得将这个每次遍历的节点数量改成配置
         for( long i = 0; i < handNodeNums; i += 1000 ) {
-            List<GUID> handleGuids = vectorDAG.fetchHandleGuidsByTaskPriority(offset, 1000);
+            List<GUID> handleGuids = this.mLayerInstrument.fetchSourceGuidsByTaskPriority(vectorDAG.getAffiliateLayerGuid(),offset, 1000);
             for (GUID handleGuid : handleGuids) {
                 TaskElement taskElement = this.mRuntimeAtlasInstrument.queryTaskElementByGuid(handleGuid);
                 if (taskElement.getPriority() > this.mnPriority) {
-                    this.bfsGraph(vectorDAG,this.mnPriority);
+                    this.bfsGraph(vectorDAG,this.mnPriority,layer.getSinkGuids());
                     ++this.mnPriority;
                 }
                 GenericStratumQueueElement element = new GenericStratumQueueElement();
@@ -50,33 +57,44 @@ public class InDegreeFirstStrategy implements GraphPriorityProcessStrategy {
                 ++offset;
             }
         }
-        this.bfsGraph( vectorDAG, this.mnPriority );
+        // 跳出循环后要将所有节点入队，直接降低成最低优先级
+        this.bfsGraph( vectorDAG, 10,layer.getSinkGuids() );
     }
 
-    protected void bfsGraph( VectorDAG vectorDAG, int priority ) {
-        while( !this.mTempMegaStratumQueue.isEmpty() ) {
+    protected void bfsGraph(VectorDAG vectorDAG, int priority, List<GUID> sinkNodes) {
+        while (!this.mTempMegaStratumQueue.isEmpty()) {
             QueueStratumElement pop = this.mTempMegaStratumQueue.popFront();
+            GUID currentNodeGuid = pop.getObjectGuid();
 
-            TaskElement taskElement = this.mRuntimeAtlasInstrument.queryTaskElementByGuid(pop.getObjectGuid());
+            TaskElement taskElement = this.mRuntimeAtlasInstrument.queryTaskElementByGuid(currentNodeGuid);
             GenericQueueElement element = new GenericQueueElement();
-            element.setObjectGuid( pop.getObjectGuid() );
+            element.setObjectGuid(currentNodeGuid);
             element.setPriority(taskElement.getPriority());
-            this.mMegaDeflectPriorityQueue.pushBack( element );
+            this.mMegaDeflectPriorityQueue.pushBack(element);
 
-            this.mRuntimeAtlasInstrument.putStratumMeta( vectorDAG.getGuid(), (short) pop.getStratum(), (short) element.getPriority(), this.mMegaDeflectPriorityQueue.getSegmentName() );
+            this.mRuntimeAtlasInstrument.putStratumMeta(
+                    vectorDAG.getAffiliateLayerGuid(),
+                    (short) pop.getStratum(),
+                    (short) element.getPriority(),
+                    this.mMegaDeflectPriorityQueue.getSegmentName()
+            );
 
-            long childNodeNum = vectorDAG.countChildNodeNum(pop.getObjectGuid());
+            if (sinkNodes != null && sinkNodes.contains(currentNodeGuid)) {
+                continue;
+            }
+
+            long childNodeNum = vectorDAG.countChildNodeNum(currentNodeGuid);
             long childOffset = 0;
 
-            for( int i = 0; i < childNodeNum; i+=1000 ) {
-                List<GUID> guids = vectorDAG.fetchChildNodeGuids(childOffset, 1000, pop.getObjectGuid());
-                for( GUID guid : guids ) {
+            for (int i = 0; i < childNodeNum; i += 1000) {
+                List<GUID> guids = vectorDAG.fetchChildNodeGuids(childOffset, 1000, currentNodeGuid);
+                for (GUID guid : guids) {
                     TaskElement childtaskElement = this.mRuntimeAtlasInstrument.queryTaskElementByGuid(guid);
-                    if( childtaskElement.getPriority() <= priority  ) {
+                    if (childtaskElement.getPriority() <= priority) {
                         GenericStratumQueueElement stratumQueueElement = new GenericStratumQueueElement();
-                        stratumQueueElement.setObjectGuid( guid );
+                        stratumQueueElement.setObjectGuid(guid);
                         stratumQueueElement.setStratum((short) (pop.getStratum() + 1));
-                        this.mTempMegaStratumQueue.pushBack( stratumQueueElement );
+                        this.mTempMegaStratumQueue.pushBack(stratumQueueElement);
                     }
                 }
             }
--- Odin/odin-framework-runtime/src/main/java/com/walnut/odin/conduct/RavenCollectiveTaskRegiment.java
+++ Odin/odin-framework-runtime/src/main/java/com/walnut/odin/conduct/RavenCollectiveTaskRegiment.java
@@ -1,19 +1,100 @@
 package com.walnut.odin.conduct;
 
+import com.pinecone.hydra.atlas.advance.GenericTapedBFSGraphAdvancer;
+import com.pinecone.hydra.atlas.advance.strategy.AtlasPriorityProcessStrategy;
+import com.pinecone.hydra.atlas.advance.strategy.InDegreeFirstStrategy;
+import com.pinecone.hydra.atlas.graph.RuntimeAtlasInstrument;
+import com.pinecone.hydra.orchestration.ParallelAction;
 import com.pinecone.hydra.proc.ProcessManager;
 import com.pinecone.hydra.system.Hydrogen;
+import com.pinecone.hydra.system.ko.driver.KOIMappingDriver;
+import com.pinecone.hydra.unit.iqueue.MagnitudeDPQueue;
+import com.pinecone.hydra.unit.iqueue.MegaDPStratumQueue;
+import com.pinecone.hydra.unit.iqueue.QueueTableMeta;
+import com.pinecone.hydra.unit.vgraph.VectorDAG;
+import com.pinecone.hydra.unit.vgraph.layer.Layer;
+import com.pinecone.hydra.unit.vgraph.layer.LayerInstrument;
 import com.walnut.odin.task.CentralizedTaskInstrument;
 import com.walnut.odin.ups.conduct.TaskRegimentDomain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RavenCollectiveTaskRegiment implements CollectiveTaskRegiment {
 
     protected Hydrogen                  mSystem;
 
-    protected CentralizedTaskInstrument mTaskInstrument;
+    protected CentralizedTaskInstrument     mTaskInstrument;
 
-    protected ProcessManager            mProcessManager;
+    protected ProcessManager                mProcessManager;
 
-    protected TaskRegimentDomain        mTaskRegimentDomain;
+    protected TaskRegimentDomain            mTaskRegimentDomain;
+
+    private VectorDAG                       mVectorDAG;
+
+    private LayerInstrument                 mLayerInstrument;
+
+    private RuntimeAtlasInstrument          mRuntimeAtlasInstrument;
+
+    private KOIMappingDriver                mQueueDriver;
+
+    private ParallelAction              mParallelAction;
+
+    private long                        mnCurrentPos;
+
+    private long                        mnBatchSize;
+
+    private List<VectorDAG> mExecuteGraph;
+
+    public RavenCollectiveTaskRegiment(VectorDAG vectorDAG, LayerInstrument layerInstrument,
+                                      long batchSize, RuntimeAtlasInstrument runtimeAtlasInstrument,KOIMappingDriver queueDriver) {
+        this.mVectorDAG                 = vectorDAG;
+        this.mLayerInstrument           = layerInstrument;
+        this.mRuntimeAtlasInstrument    = runtimeAtlasInstrument;
+        this.mParallelAction            = new ParallelAction();
+        this.mnCurrentPos               = 0;
+        this.mnBatchSize                = batchSize;
+        this.mQueueDriver               = queueDriver;
+    }
+
+    @Override
+    public void execute() {
+        // 将图分解为可执行的子图
+        this.createExecuteGraph();
+
+        // 对每个子图生成最终执行队列
+        for( VectorDAG vectorDAG : this.mExecuteGraph ) {
+            this.createExecuteQueue( vectorDAG );
+        }
+
+        // 将队列中生成的节点转换成执行任务加入执行器
+
+    }
+
+    private void createExecuteGraph() {
+        List<Layer> layers = this.mLayerInstrument.splitGraphLayer(this.mVectorDAG);
+        ArrayList<VectorDAG> vectorDAGS = new ArrayList<>();
+        for( Layer layer : layers ) {
+            VectorDAG vectorDAG = this.mRuntimeAtlasInstrument.toVectorDAG(layer);
+            vectorDAGS.add( vectorDAG );
+        }
+        this.mExecuteGraph = vectorDAGS;
+    }
+
+    private void createExecuteQueue( VectorDAG vectorDAG ) {
+        QueueTableMeta meta1 = new QueueTableMeta();
+        meta1.setQueueTableName( "hydra_queue_nodes" );
+        QueueTableMeta meta2 = new QueueTableMeta();
+        meta2.setQueueTableName( "hydra_temporary_queue_nodes" );
+
+        MagnitudeDPQueue magnitudeDPQueue = new MagnitudeDPQueue(this.mQueueDriver, 0, "segment_name", vectorDAG.getAffiliateLayerGuid().toString(), meta1);
+        MegaDPStratumQueue megaDPStratumQueue = new MegaDPStratumQueue(this.mQueueDriver, "segment_name", vectorDAG.getAffiliateLayerGuid().toString(), meta2);
+
+        AtlasPriorityProcessStrategy strategy = new AtlasPriorityProcessStrategy();
+        strategy.addStrategy( new InDegreeFirstStrategy(this.mRuntimeAtlasInstrument, magnitudeDPQueue, megaDPStratumQueue,this.mLayerInstrument) );
+        GenericTapedBFSGraphAdvancer advancer = new GenericTapedBFSGraphAdvancer(this.mRuntimeAtlasInstrument, magnitudeDPQueue,strategy);
+        advancer.traverse( vectorDAG );
+    }
 
 
 

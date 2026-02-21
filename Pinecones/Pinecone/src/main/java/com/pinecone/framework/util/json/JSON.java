c7d4b57076b14767fbfab5cf95c6fe7ae9b0f9e7
undefined <bean@rednest.cn>
2026-02-22 01:56:56 +0800

    [Feat] New improved Heist Architecture.

--- Pinecones/Pinecone/src/main/java/com/pinecone/framework/util/json/JSON.java
+++ Pinecones/Pinecone/src/main/java/com/pinecone/framework/util/json/JSON.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringWriter;
 
+import com.pinecone.framework.system.Experimental;
+
 public final class JSON {
     public static final Object NULL      = new JSON.Null();
 
@@ -37,6 +39,24 @@ public final class JSON {
         ObjectJSONCursorUnmarshal unmarshal = new ObjectJSONCursorUnmarshal( reader, classType );
         return classType.cast( unmarshal.nextValue() ) ;
     }
+
+    @Experimental
+    @SuppressWarnings( "unchecked" )
+    public static <T> T unmarshal ( String szJsonString, TypeReference<T > type ) {
+        Class<T > classType = (Class<T>) type.getType().getClass();
+        ObjectJSONCursorUnmarshal unmarshal = new ObjectJSONCursorUnmarshal( szJsonString, classType );
+        return classType.cast( unmarshal.nextValue() ) ;
+    }
+
+    @Experimental
+    @SuppressWarnings( "unchecked" )
+    public static <T> T unmarshal ( Reader reader, TypeReference<T > type ) {
+        Class<T > classType = (Class<T>) type.getType().getClass();
+        ObjectJSONCursorUnmarshal unmarshal = new ObjectJSONCursorUnmarshal( reader, classType );
+        return classType.cast( unmarshal.nextValue() ) ;
+    }
+
+
 
     public static String encode    ( Object that, JSONEncoder encoder ) {
         return JSON.encode( that, 0, encoder );
---
+++ Pinecones/Pinecone/src/main/java/com/pinecone/framework/util/json/TypeReference.java
@@ -0,0 +1,27 @@
+package com.pinecone.framework.util.json;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public abstract class TypeReference<T> implements Comparable<TypeReference<T>> {
+    protected final Type _type;
+
+    protected TypeReference() {
+        Type superClass = this.getClass().getGenericSuperclass();
+        if (superClass instanceof Class) {
+            throw new IllegalArgumentException("Internal error: TypeReference constructed without actual type information");
+        }
+        else {
+            this._type = ((ParameterizedType)superClass).getActualTypeArguments()[0];
+        }
+    }
+
+    public Type getType() {
+        return this._type;
+    }
+
+    @Override
+    public int compareTo(TypeReference<T> o) {
+        return 0;
+    }
+}

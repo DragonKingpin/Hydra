b862572b51835647e6f145d35f41bd44ab03fceb
undefined <bean@rednest.cn>
2026-05-18 02:32:47 +0800

    [Feat] New Improved Integration Kernel Architecture.

--- Odin/odin-architecture/src/main/java/com/walnut/odin/system/TaskCentralControl.java
+++ Odin/odin-architecture/src/main/java/com/walnut/odin/system/TaskCentralControl.java
@@ -2,6 +2,18 @@ package com.walnut.odin.system;
 
 import com.pinecone.framework.system.SynergicSystem;
 import com.pinecone.hydra.system.centrum.CentralControlSubsystem;
+import com.pinecone.hydra.unit.vgraph.layer.LayerInstrument;
+import com.walnut.odin.atlas.graph.RuntimeAtlasInstrument;
+import com.walnut.odin.conduct.CollectiveTaskRegiment;
+import com.walnut.odin.conduct.schedule.UniformTaskScheduler;
 
 public interface TaskCentralControl extends SynergicSystem, CentralControlSubsystem {
+
+    LayerInstrument layerInstrument();
+
+    RuntimeAtlasInstrument atlasInstrument();
+
+    CollectiveTaskRegiment taskRegiment();
+
+    UniformTaskScheduler taskScheduler();
 }
